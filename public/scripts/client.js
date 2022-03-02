5f8e7d88d8e25244def158388b8514a4eba42f28
trijaychan <trijaychan@gmail.com>
2022-03-02 20:28:37 +0000

    script now allow tweet submittion and displays adds them to tweet container dynamically

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 const renderTweets = (tweets) => {
   for (const tweet of tweets) {
     $("#tweets-container").append(createTweetElement(tweet));
@@ -40,14 +15,14 @@ const createTweetElement = (tweetInfo) => {
                     <header>
                       <div class ="tweet-top">
                         <div class="user-info">
-                          <img src="${tweetInfo.user.avatars}" style="height: 75px;"> 
+                          <img src="${tweetInfo.user.avatars}"> 
                           <p>${tweetInfo.user.name}</p>
                         </div>
                         <p><strong>${tweetInfo.content.text}</strong></p>
                       </div>
                       <hr>
                       <div class="tweet-bottom">
-                        <p><strong>${timeago.format(tweetInfo.created_at, 7)} days ago</strong></p>
+                        <p><strong>${timeago.format(tweetInfo.created_at)}</strong></p>
                         <div>
                           <i class="fa-solid fa-flag fa-xs"></i>
                           <i class="fa-solid fa-retweet fa-xs"></i>
@@ -57,11 +32,29 @@ const createTweetElement = (tweetInfo) => {
                     </header>
                   </article>
                   <br>`);
-                  
+
   return $tweet;
+};
+const loadTweets = () => {
+  $.ajax("/tweets/", { method: "GET" })
+    .then((data) => {
+      renderTweets(data);
+    });
 };
 
 
 $(document).ready(function() {
-  renderTweets(data);
+  loadTweets();
+
+  $("#compose-tweet").submit(function(event) {
+    const input = "";
+
+    if (!input) {
+      alert("Please enter a valid tweet!");
+    } else {
+      $.post("/tweets/", $(this).serialize());
+    }
+
+    event.preventDefault();
+  });
 });
