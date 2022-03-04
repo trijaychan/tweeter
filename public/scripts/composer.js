511887fd96a20d65e75827e78a0eb5afc5c7f84c
trijaychan <trijaychan@gmail.com>
2022-03-04 03:16:00 +0000

    moved all events for composer button to composer.js

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
+  $("#scroll-to-compose").click(function() {
+    $([document.documentElement, document.body]).animate({
+      scrollTop: $(".new-tweet").offset().top - 150
+    }, 500);
+    $("#tweet-text").focus();
+    $("#scroll-to-compose").hide();
+  });
+
+  $("#tweet-text").focusout(function() {
+    $("#scroll-to-compose").show();
+  });
+
   $("#scroll-to-top").hide();
 
   $("#scroll-to-top").click(function() {
@@ -10,10 +22,20 @@ $(document).ready(function() {
   $(window).scroll(function() {
     if ($(window).scrollTop() > 0) {
       $("#scroll-to-top").show();
-      $("#scroll-to-compose").hide();
     } else {
       $("#scroll-to-top").hide();
-      $("#scroll-to-compose").show();
     }
   })
+
+  $("#tweet-text")[0].addEventListener("keyup", function() {
+    const counter = $("output[for='" + $(this).attr("id") + "']");
+
+    $(counter).html(140 - this.value.length);
+
+    if (this.value.length > 140) {
+      $(counter).css("color", "red");
+    } else {
+      $(counter).css("color", "#58544C");
+    }
+  });
 });
